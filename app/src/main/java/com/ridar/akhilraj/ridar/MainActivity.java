a50f310f00e9e9873f94fb60c70ecd0c072c18b1
Akhil Raj Azhikodan <akhil95raj@gmail.com>
2016-07-16 14:52:59 +0530

    HERE setup
    The basic map setup

---
+++ app/src/main/java/com/ridar/akhilraj/ridar/MainActivity.java
@@ -0,0 +1,74 @@
+package com.ridar.akhilraj.ridar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.here.android.mpa.common.GeoCoordinate;
+import com.here.android.mpa.common.OnEngineInitListener;
+import com.here.android.mpa.mapping.Map;
+import com.here.android.mpa.mapping.MapFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Map map = null;
+    private MapFragment mapFragment = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Search for the map fragment to finish setup by calling init().
+        mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.mapfragment);
+
+        mapFragment.init(new OnEngineInitListener() {
+            @Override
+            public void onEngineInitializationCompleted(
+                    OnEngineInitListener.Error error)
+            {
+                if (error == OnEngineInitListener.Error.NONE) {
+                    // retrieve a reference of the map from the map fragment
+                    map = mapFragment.getMap();
+                    // Set the map center to the Vancouver region (no animation)
+                    map.setCenter(new GeoCoordinate(49.196261, -123.004773, 0.0),
+                            Map.Animation.NONE);
+                    // Set the zoom level to the average between min and max
+                    map.setZoomLevel(
+                            (map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
+
+                    Log.d("MAP:", "Initialized");
+                } else {
+                    System.out.println("ERROR: Cannot initialize Map Fragment");
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
