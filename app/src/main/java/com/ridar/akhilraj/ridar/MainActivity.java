5b5b4bd52f7083fc1b2c92594b26f125e1d7ea60
Akhil Raj Azhikodan <akhil95raj@gmail.com>
2016-07-17 07:35:51 +0530

    navigation from current location, destination search

--- app/src/main/java/com/ridar/akhilraj/ridar/MainActivity.java
+++ app/src/main/java/com/ridar/akhilraj/ridar/MainActivity.java
@@ -1,22 +1,40 @@
 package com.ridar.akhilraj.ridar;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.here.android.mpa.common.GeoCoordinate;
 import com.here.android.mpa.common.GeoPosition;
 import com.here.android.mpa.common.OnEngineInitListener;
 import com.here.android.mpa.common.PositioningManager;
-import com.here.android.mpa.common.RoadElement;
 import com.here.android.mpa.guidance.NavigationManager;
 import com.here.android.mpa.mapping.Map;
 import com.here.android.mpa.mapping.MapFragment;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 import android.view.View;
 import android.widget.Toast;
@@ -29,32 +47,50 @@ import com.here.android.mpa.routing.RoutingError;
 import com.here.android.mpa.routing.RouteOptions;
 import com.here.android.mpa.routing.RoutePlan;
 import com.here.android.mpa.routing.RouteResult;
+import com.here.android.mpa.search.ErrorCode;
+import com.here.android.mpa.search.GeocodeRequest;
+import com.here.android.mpa.search.Location;
+import com.here.android.mpa.search.ResultListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private  int LEDSTRAIGHT = 9;
+    private  int LEDDOWN = 10;
+    private  int LEDLEFT = 11;
+    private  int LEDRIGHT = 12;
+    private  int LEDSLIGHTLEFT = 13;
+    private  int LEDSLIGHTRIGHT = 14;
+    private  int LEDHARDLEFT = 15;
+    private  int LEDHARDRIGHT = 16;
 
     // map embedded in the map fragment
     private Map map = null;
-
     // map fragment embedded in this activity
     private MapFragment mapFragment = null;
-
     // TextView for displaying the current map scheme
     private TextView textViewResult = null;
-
     // MapRoute for this activity
     private MapRoute mapRoute = null;
 
-    private long simulationSpeed = 10;
-
     private  NavigationManager navigationManager = null;
-
+    private PositioningManager positionManager = null;
     private long EDA = -1;
-
     private Boolean StateBlink = false;
     private Boolean StateSolid = false;
+    private Boolean KeepSideFlag = false;
+    private int LEDTOBLINK = 0;
+    private ResultListener<List<Location>> listener=null;
+    private GeocodeRequest request = null;
+    private void SendBluetoohSignal(int signal){
+    }
+    private RequestQueue mRequestQueue;
+    private EditText placeEditText;
 
-    private 
 
 
     @Override
@@ -65,7 +101,7 @@ public class MainActivity extends AppCompatActivity {
         textViewResult = (TextView) findViewById(R.id.title);
         textViewResult.setText(R.string.textview_routecoordinates_2waypoints);
 
-
+        placeEditText = (EditText) findViewById(R.id.placeEditText);
 
         //Map Setup
         mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.mapfragment);
@@ -83,13 +119,43 @@ public class MainActivity extends AppCompatActivity {
                     map.setZoomLevel((map.getMaxZoomLevel() + map.getMinZoomLevel()) / 2);
                     map.getPositionIndicator().setVisible(true);
                     navigationManager = NavigationManager.getInstance();
+
+                    positionManager = PositioningManager.getInstance();
+                    positionManager.start(PositioningManager.LocationMethod.GPS_NETWORK);
+                    positionManager.addListener(new WeakReference<PositioningManager.OnPositionChangedListener>(positionList));
+
+
                 } else {
                     System.out.println("ERROR: Cannot initialize Map Fragment");
                 }
             }
         });
 
+
+
+
+
+
     }
+
+
+    private PositioningManager.OnPositionChangedListener positionList = new
+            PositioningManager.OnPositionChangedListener() {
+
+                public void onPositionUpdated(PositioningManager.LocationMethod method,
+                                              GeoPosition position, boolean isMapMatched) {
+                    // set the center only when the app is in the foreground
+                    // to reduce CPU consumption
+
+                        map.setCenter(position.getCoordinate(),
+                                Map.Animation.NONE);
+
+                }
+
+                public void onPositionFixChanged(PositioningManager.LocationMethod method,
+                                                 PositioningManager.LocationStatus status) {
+                }
+    };
 
     private NavigationManager.NewInstructionEventListener newInstructionEventListener = new NavigationManager.NewInstructionEventListener() {
 
@@ -99,19 +165,65 @@ public class MainActivity extends AppCompatActivity {
             if (maneuver != null) {
                 if (maneuver.getAction() == Maneuver.Action.END) {
                     //notify the user that the route is complete
+                }else {
+
+                    KeepSideFlag = false;
+                    LEDTOBLINK = 0;
+                    String maneuvarGetTurn = maneuver.getTurn().toString();
+                    Log.d("DBGRIDAR:", "GetTurn-" + maneuvarGetTurn);
+                    Log.d("DBGRIDAR:", "DistanceToNextManeuvar-" + String.valueOf(maneuver.getDistanceToNextManeuver()));
+                    Log.d("DBGRIDAR:", "Coordinate-" + maneuver.getCoordinate().toString());
+                    Log.d("DBGRIDAR", "NextRoadName-" + maneuver.getNextRoadName());
+
+                    if (maneuvarGetTurn.equals("LIGHT_RIGHT")) {
+                        LEDTOBLINK = LEDSLIGHTRIGHT;
+                    } else if (maneuvarGetTurn.equals("QUITE_RIGHT")) {
+                        LEDTOBLINK = LEDRIGHT;
+                    } else if (maneuvarGetTurn.equals("HEAVY_RIGHT")) {
+                        LEDTOBLINK = LEDHARDRIGHT;
+                    } else if (maneuvarGetTurn.equals("LIGHT_LEFT")) {
+                        LEDTOBLINK = LEDSLIGHTLEFT;
+                    } else if (maneuvarGetTurn.equals("QUITE_LEFT")) {
+                        LEDTOBLINK = LEDLEFT;
+                    } else if (maneuvarGetTurn.equals("HEAVY_LEFT")) {
+                        LEDTOBLINK = LEDHARDLEFT;
+                    } else if (maneuvarGetTurn.toLowerCase().contains("roundabout")) {
+                        Log.d("DBGRIDAR:", "ANGLE" + String.valueOf(maneuver.getAngle()));
+                        long turnangle = maneuver.getAngle();
+
+                        if (turnangle < 68) {
+                            LEDTOBLINK = LEDHARDLEFT;
+                        } else if (turnangle < 113) {
+                            LEDTOBLINK = LEDLEFT;
+                        } else if (turnangle < 158) {
+                            LEDTOBLINK = LEDSLIGHTLEFT;
+                        } else if (turnangle < 203) {
+                            LEDTOBLINK = LEDSTRAIGHT;
+                        } else if (turnangle < 248) {
+                            LEDTOBLINK = LEDRIGHT;
+                        } else if (turnangle < 293) {
+                            LEDTOBLINK = LEDSLIGHTRIGHT;
+                        } else if (turnangle < 293) {
+                            LEDTOBLINK = LEDHARDRIGHT;
+                        }
+                    } else if (maneuvarGetTurn.equals("KEEP_RIGHT")) {
+                        LEDTOBLINK = LEDRIGHT;
+                        KeepSideFlag = true;
+                    } else if (maneuvarGetTurn.equals("KEEP_LEFT")) {
+                        LEDTOBLINK = LEDLEFT;
+                        KeepSideFlag = true;
+                    }
+
+
+                    Log.d("DBGRIDAR", "SIGNAL-STOP");
+                    SendBluetoohSignal(0);
+                    StateBlink = false;
+                    StateSolid = false;
                 }
-
-                Log.d("NAVIGATION:GETTURN :", maneuver.getTurn().toString());
-                Log.d("NAVIGATION:DISTTONXTM:", String.valueOf(maneuver.getDistanceToNextManeuver()));
-                Log.d("NAVIGATION:COORD:",maneuver.getCoordinate().toString());
-                Log.d("NAVIGATION:Road",maneuver.getNextRoadName());
-
-                Log.d("SIGNAL","STOP");
-                StateBlink = false;
-                StateSolid = false;
             }
         }
     };
+
 
     private NavigationManager.PositionListener positionListener = new NavigationManager.PositionListener() {
         @Override
@@ -121,18 +233,21 @@ public class MainActivity extends AppCompatActivity {
             map.setCenter(geoPosition.getCoordinate(), Map.Animation.NONE);
 
             EDA = navigationManager.getNextManeuverDistance();
-            Log.d("EDA:",String.valueOf(EDA));
+            //Log.d("DBGRIDAR:",String.valueOf(EDA));
 
 
             if(StateSolid == false){
-                if(EDA<50){
+                if(EDA<50 && KeepSideFlag == false){
                    //SOLID
-                    Log.d("SIGNAL:","SIGNALSOLID");
+                    LEDTOBLINK = LEDTOBLINK-8;
+                    Log.d("DBGRIDAR:","SIGNALSOLID - "+LEDTOBLINK);
+                    SendBluetoohSignal(LEDTOBLINK);
                     StateBlink = false;
                     StateSolid = true;
                }else if(EDA<200 && StateBlink==false){
                    //BLINK
-                    Log.d("SIGNAL:","SIGNALBLINK");
+                    Log.d("DBGRIDAR:","SIGNALBLINK - "+LEDTOBLINK);
+                    SendBluetoohSignal(LEDTOBLINK);
                     StateBlink = true;
                }
             }
@@ -173,6 +288,14 @@ public class MainActivity extends AppCompatActivity {
 
             };
 
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
     // Functionality for taps of the "Get Directions" button
     public void getDirections(View view) {
         // 1. clear previous results
@@ -182,26 +305,14 @@ public class MainActivity extends AppCompatActivity {
             mapRoute = null;
         }
 
-        // 2. Initialize CoreRouter
-        CoreRouter coreRouter = new CoreRouter();
+        String enteredPlace = placeEditText.getText().toString();
+        JsonObjectRequest placeLocationRequest = getJsonObjectRequest(enteredPlace);
+        RequestQueue rq = getRequestQueue();
+        rq.add(placeLocationRequest);
+        rq.start();
 
-        // 3. Select routing options via RoutingMode
-        RoutePlan routePlan = new RoutePlan();
 
-        RouteOptions routeOptions = new RouteOptions();
-        routeOptions.setTransportMode(RouteOptions.TransportMode.CAR);
-        routeOptions.setRouteType(RouteOptions.Type.FASTEST);
-        routePlan.setRouteOptions(routeOptions);
 
-        // 4. Select Waypoints for your routes
-        // START: BC Place Stadium
-        routePlan.addWaypoint(new GeoCoordinate(13.0311221,77.562976));
-
-        // END: Airport, YVR
-        routePlan.addWaypoint(new GeoCoordinate(13.1187344,77.5765951));
-
-        // 5. Retrieve Routing information via CoreRouter.Listener
-        coreRouter.calculateRoute(routePlan, routerListener);
     };
 
     @Override
@@ -225,4 +336,50 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    private JsonObjectRequest getJsonObjectRequest(String place) {
+        String placeUrl ="https://maps.googleapis.com/maps/api/geocode/json?address=" + place + "&key=AIzaSyD-9JcB-zqw7hlQ8NE4Th07y-c1Sm6BAIE";
+        return new JsonObjectRequest(placeUrl, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray resultsArray = response.getJSONArray("results");
+                            JSONObject firstPlace = resultsArray.getJSONObject(0);
+                            JSONObject geometry = firstPlace.getJSONObject("geometry");
+                            JSONObject location = geometry.getJSONObject("location");
+                            Double lat = location.getDouble("lat");
+                            Double lng = location.getDouble("lng");
+                            onPlaceLocationReceived(lat, lng);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                });
+    }
+
+    private void onPlaceLocationReceived(Double lat, Double lng) {
+        Log.d("MainActivity", "Lat" + lat + "Lng" + lng);
+
+        CoreRouter coreRouter = new CoreRouter();
+        RoutePlan routePlan = new RoutePlan();
+        RouteOptions routeOptions = new RouteOptions();
+        routeOptions.setTransportMode(RouteOptions.TransportMode.CAR);
+        routeOptions.setRouteType(RouteOptions.Type.FASTEST);
+        routePlan.setRouteOptions(routeOptions);
+        Log.d("DBGRIDART", positionManager.getPosition().getCoordinate().toString());
+        routePlan.addWaypoint(new GeoCoordinate(positionManager.getPosition().getCoordinate()));
+        routePlan.addWaypoint(new GeoCoordinate(lat,lng)); //VidhanSoudha
+        coreRouter.calculateRoute(routePlan, routerListener);
+    }
 }
+
